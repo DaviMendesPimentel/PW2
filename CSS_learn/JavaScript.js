f7f6822a74002d213662285ef70b5b4cbebcdac6
eclonit <davizinhozl.mendes@gmail.com>
2018-08-16 22:44:55 -0300

    atualização 2

---
+++ CSS_learn/JavaScript.js
@@ -0,0 +1,30 @@
+function cube(x)
+{
+	return x * x * x;
+}
+
+function MyFunction()
+			{
+					var i = 0;
+					document.getElementById("demo").innerHTML = "paragrafo MUDADO PORRA, AEEEE\n uhulll!!";
+					
+					document.write("O Número inserido foi ");
+					i = prompt("Insira um numero: ");
+					document.write(i);
+					document.write("o cubo deste número é: ");
+					document.write(cube(i));
+			}
+
+
+
+/*
+function MyFunction()
+{
+	console.log("function MyFunction(){... rodou}");
+	document.getElementById('demo').innerHTML = "javascript rodou";
+}
+function outraFuncao()
+{
+	console.log("vc so vai ver isso no console se chamar a funcao");
+}
+*/
