a0caed3ae921de510c1bf0972d419a9957047818
eclonit <davizinhozl.mendes@gmail.com>
2018-08-30 22:35:27 -0300

    Atualização 5

--- CSS_learn/JS_PW_Test.js
+++ CSS_learn/JS_PW_Test.js
@@ -1,11 +1,17 @@
 
-function JavaScriptMerda(bananaFood, potato)
+function print_Any_Val(array_val)
 {
-	return bananaFood * potato;
+	document.write(array_val);
 }
-function Soma()
+function main()
 {
-	var jailson_mendes = JavaScriptMerda(10, 6);
-	document.write("Java script do caralho mesmo viu\n");
-	alert(jailson_mendes);
+	var anyVal = ["Zero", "Um", "Dois", "Tres", "Quatro", "Cinco"];
+	var i = 0;
+	
+	while(i == 0){
+		var j = 0;
+		for(j = 0; j < 6; j++){
+			print_Any_Val(anyVal[j]);
+		}
+	}
 }
